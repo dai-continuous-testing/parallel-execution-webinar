55e4f7040f75566b3ff225a4fd3b008b4c6fb0d3
tom <tom_p@experitest.com>
2019-01-09 18:12:02 +0200

    first commit

---
+++ src/main/java/TestLifeCycle2.java
@@ -0,0 +1,59 @@
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.annotations.*;
+
+public class TestLifeCycle2 {
+
+    public final Logger log = LoggerFactory.getLogger(this.getClass().getName());
+
+    @BeforeSuite
+    public void setUpSuite() {
+        log.info( " THREAD ID : {} | Entering Setup {} function!", Thread.currentThread().getId(),"Suite");
+    }
+
+    @BeforeTest
+    public void setUpTest() {
+        log.info( " THREAD ID : {} | Entering Setup {} function!", Thread.currentThread().getId(),"Test");
+    }
+
+    @BeforeClass
+    public void setUpClass() {
+        log.info( " THREAD ID : {} | Entering Setup {} function!", Thread.currentThread().getId(),"Class");
+    }
+
+    @BeforeMethod
+    public void setUpMethod() {
+        log.info( " THREAD ID : {} | Entering Setup {} function!", Thread.currentThread().getId(),"Method");
+    }
+
+    @Test
+    public void test1() {
+        log.info( " THREAD ID : {} | Entering {} function!",Thread.currentThread().getId(),"test1");
+    }
+    @Test
+    public void test2() {
+        log.info( " THREAD ID : {} | Entering {} function!",Thread.currentThread().getId(),"test2");
+    }
+
+    @AfterMethod
+    public void tearDownMethod() {
+        log.info( " THREAD ID : {} | Entering After {} function!", Thread.currentThread().getId(),"Method");
+    }
+
+    @AfterClass
+    public void tearDownClass() {
+        log.info( " THREAD ID : {} | Entering After {} function!", Thread.currentThread().getId(),"Class");
+    }
+
+    @AfterTest
+    public void tearDownTest() {
+        log.info( " THREAD ID : {} | Entering After {} function!", Thread.currentThread().getId(),"Test");
+    }
+
+    @AfterSuite
+    public void tearDownSuite() {
+        log.info( " THREAD ID : {} | Entering After {} function!", Thread.currentThread().getId(),"Suite");
+    }
+
+
+}
